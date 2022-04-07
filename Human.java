572534cb81f6db19e8c28b2200c5ed77cb8e190b
Park Sangho <96759729+Whitesharkbear@users.noreply.github.com>
2022-04-07 09:07:47 +0900

    Add files via upload

--- Human.java
+++ Human.java
@@ -1,8 +1,20 @@
-package Alsong;
+package Test;
 
-//사람 클래스
+//사람 클래스(Ver.2)
 public class Human {
-	String name;	// 이름
-	int height;		// 신장
-	int weight;		// 체중
-}
+	private String name;	//이름
+	private int height;		//신장
+	private int weight;		//체중
+
+	//--생성자 --//
+	Human(String n, int h, int w){
+		name = n; height = h; weight=w;
+	}
+	
+	String getName() { return name;}	//이름 가져오기
+	int getHeight()  { return height;}	//신장 불러오기
+	int getWeight()	 { return weight;}	//체중 불러오기
+	
+	void gainWeight(int w)	{weight += w;}//살이 찐다
+	void reduceWeight(int w) {weight -= w;}//살이 빠진다
+	}
--- VarTest1.java
+++ VarTest1.java
@@ -1,14 +1,10 @@
-package ch3;
+package ch33;
 
 public class VarTest1 {
 
 	public static void main(String[] args) {
-		int a=10, b;
-		b=a;
-		System.out.println("a="+a);
-		System.out.println("b="+b);	
-		System.out.println(a+"+"+b+"="+(a+b));
-		
+		// TODO Auto-generated method stub
+
 	}
 
 }
